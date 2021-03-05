a467f2eac1d1c2355826ffaabe4e01fc1413a0bf
Akshat Dobriyal <56156498+AkshatDobriyal@users.noreply.github.com>
2021-03-05 23:30:54 +0530

    Delete bloggingWebApp directory

--- bloggingWebApp/members/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import UserRegistrationView
-
-urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name = 'register'),
-]
--- bloggingWebApp/myblog/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Post, Category
-# Register your models here.
-
-admin.site.register(Post)
-admin.site.register(Category)
