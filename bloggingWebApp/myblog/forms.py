ff6b9de31f36e390cd8cd6e57013c6d70ca7636b
AkshatDobriyal <akcooldobriyal@gmail>
2021-03-05 22:15:10 +0530

    Add blogging webapp

---
+++ bloggingWebApp/myblog/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from .models import Post, Category
+
+#choices = [('Coding', 'Coding'), ('Sports', 'Sports'), ('Entertainment', 'Entertainment')]
+choices = Category.objects.all().values_list('name', 'name')
+choice_list = []
+
+for item in choices:
+    choice_list.append(item)
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'author', 'category', 'body')
+
+        widgets = {
+            'title' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'title'}),
+            'title_tag' : forms.TextInput(attrs={'class' : 'form-control'}),
+            'author' : forms.TextInput(attrs={'class' : 'form-control', 'value' : '', 'id' : 'user', 'type' : 'hidden'}),
+            #'author' : forms.Select(attrs={'class' : 'form-control'}),
+            'category' : forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
+            'body' : forms.Textarea(attrs={'class' : 'form-control'})
+        }
+
+class UpdatePostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'body')
+
+        widgets = {
+            'title' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
+            'title_tag' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title_Tag'}),
+            'body' : forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter the post here'})
+        }
+
+class AddCategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ('name', )
+
+        widgets = {
+            'name' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Category'}),
+        }
---
+++ bloggingWebApp/myblog/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from datetime import datetime, date
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    title_tag = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    post_date = models.DateField(auto_now_add=True)
+    category = models.CharField(max_length=255, default='coding')
+    likes = models.ManyToManyField(User, related_name='blog_posts')
+
+    def total_likes(self):
+        return self.likes.count()
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author) + ' | ' + self.category 
+    
+    def get_absolute_url(self):
+       # return reverse('post-detail', args=(str(self.id)))
+       return reverse('home')
